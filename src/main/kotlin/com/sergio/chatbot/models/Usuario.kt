8c56a3d98c695a491f18b32518bddefd5920e14a
Lener Dantas <lenerqueiroz@hotmail.com>
2025-11-18 20:47:43 -0300

    Configura banco de dados MySQL e Ktor

---
+++ src/main/kotlin/com/sergio/chatbot/models/Usuario.kt
@@ -0,0 +1,12 @@
+package com.sergio.chatbot.models
+
+import org.jetbrains.exposed.sql.Table
+
+object Usuarios : Table("Usuarios") {
+    val id = integer("id").autoIncrement()
+    val nome = varchar("nome", 255)
+    val email = varchar("email", 255).uniqueIndex()
+    val senhaHash = varchar("senha_hash", 255)
+
+    override val primaryKey = PrimaryKey(id)
+}
